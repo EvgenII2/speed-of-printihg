c4fa453952d44f1dfbf80434a79915eaa47a07f5
EvgenII L <genja1988@inbox.ru>
2021-12-13 11:51:33 +0300

    fix: deleted not use files

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Test speed of printing
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
